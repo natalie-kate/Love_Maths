f82785c4fd3a283725d8be4d3008233eb1afaa61
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-04-15 17:10:04 +0000

    Added event listener and started rungame function

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+// Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame() {
+
+    // Generate two random numbers between 1 and 25, these are our operands
+    // Math.floor rounds down to the nearest integer
+    // Math.random generates random numbers
+    // + 1 is so we don't get 0 and include 25, without it would be 0 to 24 as we're rounding down 
+
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
